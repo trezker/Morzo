40914ad12fe160c6ad7f618f7dfded11e06297a5
Anders Andersson <trezker@gmail.com>
2011-07-23 16:22:05 +0200

    Check that instances of controllers and models are only created once in controller base class

--- controllers/controller.php
+++ controllers/controller.php
@@ -4,12 +4,16 @@ class Controller
 {
 	public function Load_model($model)
 	{
+		if(isset($this->$model))
+			return;
 		require_once "models/".strtolower($model).".php";
 		$this->$model = new $model();
 	}
 
 	public function Load_controller($controller)
 	{
+		if(isset($this->$controller))
+			return;
 		require_once "controllers/".strtolower($controller).".php";
 		$this->$controller = new $controller();
 	}
