3867b2c62b963cadb6c4baba08efb939f2179a6a
Anders Andersson <trezker@gmail.com>
2012-02-10 20:13:59 +0100

    Made all views utf8

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<link rel="stylesheet" type="text/css" media="screen" href="css/style.php">
 	</head>
 	<body>
--- views/world_admin_view.php
+++ views/world_admin_view.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 		<script type="text/javascript" src="js/world_admin.js"></script>
