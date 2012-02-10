7435472a4c72e40e690dd816c711b308388b33a6
Anders Andersson <trezker@gmail.com>
2012-02-10 20:09:50 +0100

    Fixed utf8 on database connection

--- models/database.php
+++ models/database.php
@@ -10,6 +10,7 @@ if(!function_exists ('Load_database'))
 			include 'config.php';
 			$db = ADONewConnection('mysql');
 			$db->Connect($config['db_host'], $config['db_user'], $config['db_password'], $config['db_database']);
+			$db->Execute("set names 'utf8'");
 			$db->SetFetchMode(ADODB_FETCH_ASSOC);
 		}
 		return $db;
