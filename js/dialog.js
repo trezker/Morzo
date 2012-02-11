a800c19c618c063adcc15b624fa10afc01229c72
Anders Andersson <trezker@gmail.com>
2012-02-11 18:02:57 +0100

    Cleaned up the login popup

--- js/dialog.js
+++ js/dialog.js
@@ -45,3 +45,24 @@ function open_dialog(html) {
 		'background-color': 'rgba(0,0,0,0.4)'
 	});
 }
+
+function login()
+{
+	$('#openidfeedback').html('Processing...');
+	var openid = document.getElementById('openid').value;
+	$.ajax(
+	{
+		type: 'POST',
+		url: '/user/Start_openid_login',
+		data: { openid: openid },
+		success: function(data)
+		{
+			if(data.success == false) {
+				$('#openidfeedback').html('Process failed: ' + data.reason);
+			} else {
+				$('#openidfeedback').html('Redirecting');
+				window.location = data.redirect_url;
+			}
+		}
+	});
+}
--- views/login_view.php
+++ views/login_view.php
@@ -1,33 +1,8 @@
-<script type="text/javascript">
-	function login()
-	{
-		$('#openidfeedback').html('Processing...');
-		var openid = document.getElementById('openid').value;
-		$.ajax(
-		{
-			type: 'POST',
-			url: '/user/Start_openid_login',
-			data: { openid: openid },
-			success: function(data)
-			{
-				if(data.success == false) {
-					$('#openidfeedback').html('Process failed: ' + data.reason);
-				} else {
-					$('#openidfeedback').html('Redirecting');
-					window.location = data.redirect_url;
-				}
-			}
-		});
-	}
-</script>
 <div class="login" id="openid_div" style="background: #AAA; width:100%; height: 100%;">
-	<div class="login_input">
-		OpenID<br/>
+	<div class="popup_title">OpenID</div>
+	<div class="login_content">
 		<input type="text" name="openid" id="openid" />
+		<div class="login_action action" onclick="login();">Log in</div>
+		<div id="openidfeedback"></div>
 	</div>
-	<div class="action" onclick="login();">
-		Log in
-	</div>
-	</form>
-	<div id="openidfeedback"></div>
 </div>
