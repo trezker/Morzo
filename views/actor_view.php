86394844cbe6b715ca914e3c31d73cee626fd1f9
Tobias Dammers <tdammers@gmail.com>
2012-02-13 20:38:26 +0100

    Added doctypes on all top-level views

--- views/actor_view.php
+++ views/actor_view.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title><?= $actor['Name']; ?> - <?= $tab; ?> - Morzo</title> 
--- views/user_admin_view.php
+++ views/user_admin_view.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>User admin - Morzo</title>
