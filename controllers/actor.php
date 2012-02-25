f6f276accc8423743aa8a73d8970fd0262ec084d
Anders Andersson <trezker@gmail.com>
2012-02-25 17:11:38 +0100

    Moved the Get_neighbouring_locations function, it shouldn't be available public like that...

--- controllers/actor.php
+++ controllers/actor.php
@@ -57,8 +57,7 @@ class Actor extends Controller
 				if(!$travel['DestinationName'])
 					$travel['DestinationName'] = 'Unnamed location';
 			}
-			$this->Load_controller('Location');
-			$locations = $this->Location->Get_neighbouring_locations($actor_id);
+			$locations = $this->Get_neighbouring_locations($actor_id);
 			$tab_view = $this->Load_view('locations_tab_view', array('locations' => $locations, 'travel' => $travel, 'actor' => $actor), true);
 		} elseif ($tab == 'people') {
 			$actors = $this->Actor_model->Get_visible_actors($actor_id);
@@ -239,5 +238,101 @@ class Actor extends Controller
 		$success = $this->Project_model->Start_project($actor_id, $recipe_id);
 
 		echo json_encode(array('success' => $success));
+	}
+
+	private function Get_neighbouring_locations($actor_id)
+	{
+		$this->Load_controller('User');
+		if(!$this->User->Logged_in()) {
+			return;
+		}
+		$this->Load_model('Actor_model');
+		if(!$this->Actor_model->User_owns_actor($_SESSION['userid'], $actor_id)) {
+			die("This is not the actor you are looking for.");
+		}
+		$this->Load_model("Location_model");
+		$locations = $this->Location_model->Get_neighbouring_locations($actor_id);
+		$east = false;
+		$west = false;
+		$north = false;
+		$south = false;
+		foreach ($locations as &$location) {
+			if($location['x'] == 1 && $location['y'] == 0)
+				$east = true;
+			if($location['x'] == -1 && $location['y'] == 0)
+				$west = true;
+			if($location['x'] == 0 && $location['y'] == 1)
+				$south = true;
+			if($location['x'] == 0 && $location['y'] == -1)
+				$north = true;
+		}
+
+		if(!$east)
+		{
+    		$locations[] = array(
+    			'ID' => 'east',
+    			'x' => 1,
+    			'y' => 0,
+    			'Name' => 'Unnamed location'
+    		);
+		}
+		if(!$west)
+		{
+    		$locations[] = array(
+    			'ID' => 'west',
+    			'x' => -1,
+    			'y' => 0,
+    			'Name' => 'Unnamed location'
+    		);
+		}
+		if(!$south)
+		{
+    		$locations[] = array(
+    			'ID' => 'south',
+    			'x' => 0,
+    			'y' => 1,
+    			'Name' => 'Unnamed location'
+    		);
+		}
+		if(!$north)
+		{
+    		$locations[] = array(
+    			'ID' => 'north',
+    			'x' => 0,
+    			'y' => -1,
+    			'Name' => 'Unnamed location'
+    		);
+		}
+
+		foreach ($locations as &$location) {
+			if(!$location['Name'])
+				$location['Name'] = 'Unnamed location';
+			$location['Direction'] = 90+rad2deg(atan2($location['y'], $location['x']));
+			if($location['Direction'] < 0)   $location['Direction'] += 360;
+			if($location['Direction'] > 360) $location['Direction'] -= 360;
+			if($location['Direction'] < 22.5 || $location['Direction'] >= 337.5)
+				$location['Compass'] = 'N';
+			else if($location['Direction'] < 22.5+45)
+				$location['Compass'] = 'NE';
+			else if($location['Direction'] < 22.5+90)
+				$location['Compass'] = 'E';
+			else if($location['Direction'] < 22.5+135)
+				$location['Compass'] = 'SE';
+			else if($location['Direction'] < 22.5+180)
+				$location['Compass'] = 'S';
+			else if($location['Direction'] < 22.5+225)
+				$location['Compass'] = 'SW';
+			else if($location['Direction'] < 22.5+270)
+				$location['Compass'] = 'W';
+			else if($location['Direction'] < 22.5+315)
+				$location['Compass'] = 'NW';
+		}
+		function compare_direction($a, $b)
+		{
+			return $a['Direction'] > $b['Direction'];
+		}
+		unset($location);
+		usort($locations, 'compare_direction');
+		return $locations;
 	}
 }
