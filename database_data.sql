fa3df0c77890f953423210a724ffca0984d2c06a
Anders Andersson <trezker@gmail.com>
2012-03-11 14:55:53 +0100

    Improved sql backup script

--- database_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `Access` VALUES (1,'Admin');
-INSERT INTO `Count` VALUES (1,'Update',113),(2,'Max_actors',15);
-INSERT INTO `Language` VALUES (1,'English');
-INSERT INTO `Translation` VALUES (1,1,'{LNG_Actor_born}','{From_actor_name} was born.'),(2,1,'{LNG_Actor_said}','{From_actor_name} said: {Message}'),(4,1,'{LNG_Actor_whispered}','{From_actor_name} whispered to {To_actor_name}: {Message}'),(5,1,'{LNG_Actor_pointed}','{From_actor_name} pointed at {To_actor_name}'),(7,1,'{LNG_Actor_left}','{From_actor_name} left {From_location} going towards {To_location}'),(8,1,'{LNG_Actor_arrived}','{From_actor_name} arrived at {To_location} coming from {From_location}');
--- sql_backup.sh
+++ sql_backup.sh
@@ -1,2 +1,25 @@
-mysqldump --no-data --events -u root -p morzo > database_structure.sql
-mysqldump --skip-triggers --compact --no-create-info -u morzo -p morzo > database_data.sql
+#!/bin/sh
+
+
+if [ $# -eq 0 ]
+then
+	echo "Specify what to backup."
+	echo "structure, initdata or alldata."
+else
+	if [ $1 = "structure" ]
+	then
+		echo "This backup requires root access";
+		mysqldump --no-data --events -u root -p morzo > database_structure.sql
+	elif [ $1 = "initdata" ]
+	then
+		echo "This backup requires game database access";
+		mysqldump --skip-triggers --compact --no-create-info -u morzo -p morzo Access Count Language Translation > database_initial_data.sql
+	elif [ $1 = "alldata" ]
+	then
+		echo "This backup requires game database access";
+		mysqldump --skip-triggers --compact --no-create-info -u morzo -p morzo > database_all_data.sql
+	else
+		echo "Not a valid backup name."
+		echo "structure, initdata or alldata."
+	fi
+fi
