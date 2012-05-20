ff1defb226a7b5d2b3f3b85489acd8871ff9950f
Anders Andersson <trezker@gmail.com>
2012-05-20 08:26:50 +0200

    Added message when no recipes are available for a resource

--- views/recipe_selection_view.php
+++ views/recipe_selection_view.php
@@ -1,9 +1,15 @@
-<ul class="selectable" id="recipes">
-	<?php
-	$template = '<li class="action" id="recipe_{ID}" onclick="show_project_start_form('.$actor_id.', \'{ID}\')">{Name}</li>';
-	foreach ($recipe_list as $recipe) {
-		echo expand_template($template, $recipe);
-	}
-	?>
-</ul>
-<div id="view_recipe"></div>
+<?php
+if(count($recipe_list) == 0) {
+	echo "No recipes found.";
+} else {
+	echo "<ul class='selectable' id='recipes'>";
+
+		$template = '<li class="action" id="recipe_{ID}" onclick="show_project_start_form('.$actor_id.', \'{ID}\')">{Name}</li>';
+		foreach ($recipe_list as $recipe) {
+			echo expand_template($template, $recipe);
+		}
+
+	echo "
+		</ul>
+		<div id='view_recipe'></div>";
+}
