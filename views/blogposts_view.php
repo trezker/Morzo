f757a0636eb5c2e804099dbc1ef9e557208b1022
Anders Andersson <trezker@gmail.com>
2012-08-03 21:35:05 +0200

    Wiki markup on blogs

--- views/blogposts_view.php
+++ views/blogposts_view.php
@@ -1,4 +1,6 @@
 <?php
+require_once '../util/wikitexttohtml.php';
+
 $post_template = '
 		<div class="blogpost">
 			<div class="blogtitle">
@@ -6,12 +8,27 @@ $post_template = '
 				<span style="float:right; font-size: smaller;">
 					{YMD}
 				</span></div>
-			<div class="blogcontent"s>{Content}</div>
+			<div class="blogcontent"s>{!Content2}</div>
 		</div>
 	  ';
 foreach($posts as $post) {
+	// parse and display input
+	$input = explode("\n", $post['Content']);
+
+	// convert input to HTML output array
+	$output = WikiTextToHTML::convertWikiTextToHTML($input);
+	
+	// output to stream with newlines
+	$content = '';
+	foreach($output as $line) {
+		$content .= "${line}\n";
+	}
+	$post['Content2'] = $content;
+
 	list($YMD, $HMS) = explode(' ', $post['Created_date']);
 	$post['YMD'] = $YMD;
 	$post['HMS'] = $HMS;
+	//echo expand_template($post_template, $post);
 	echo expand_template($post_template, $post);
 }
+
