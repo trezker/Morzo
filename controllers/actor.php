1ce838d8734cc648e355ec2d7270e969994c365b
Anders Andersson <trezker@gmail.com>
2014-05-04 12:37:02 +0200

    Converted Cancel project

--- controllers/actor.php
+++ controllers/actor.php
@@ -642,26 +642,39 @@ class Actor extends Base {
 		}
 
 		$this->Load_model('Project_model');
-		$supply_result = $this->Project_model->Supply_project($project_id, $actor_id);
+		$success = $this->Project_model->Supply_project($project_id, $actor_id);
 		
-		return $this->Json_response_success();
+		return array(
+			'type' => 'json',
+			'data' => array(
+				'success' => $success
+			)
+		);
 	}
 
 	public function Cancel_project() {
-		header('Content-type: application/json');
-		$actor_id = $_POST['actor_id'];
-		$project_id = $_POST['project_id'];
+		$this->Load_controller('User');
+		if(!$this->User->Logged_in()) {
+			$this->Json_response_not_logged_in();
+		}
+
+		$actor_id = $this->Input_post('actor_id');
+		$project_id = $this->Input_post('project_id');
 		
 		$this->Load_model('Actor_model');
-		if(!$this->Actor_model->User_owns_actor($_SESSION['userid'], $actor_id)) {
-			echo json_encode(array('success' => false, 'reason' => 'Not your actor'));
-			return;
+		if(!$this->Actor_model->User_owns_actor($this->Session_get('userid'), $actor_id)) {
+			return $this->Json_response_not_your_actor();
 		}
 
 		$this->Load_model('Project_model');
-		$cancel_result = $this->Project_model->Cancel_project($project_id, $actor_id);
+		$success = $this->Project_model->Cancel_project($project_id, $actor_id);
 		
-		echo json_encode(array('success' => $cancel_result));
+		return array(
+			'type' => 'json',
+			'data' => array(
+				'success' => $success
+			)
+		);
 	}
 
 	public function Start_hunt() {
