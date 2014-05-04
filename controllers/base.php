ccfb06e0f4ed27e544ea9e3fecf78fd3674aef22
Anders Andersson <trezker@gmail.com>
2014-05-04 06:46:17 +0200

    Converted base controller

--- controllers/base.php
+++ controllers/base.php
@@ -1,13 +1,12 @@
 <?php
 require_once "../controllers/controller.php";
 
-class Base extends Controller
-{
+class Base extends Controller {
 	public function Before_page_load() {
 		$this->Load_controller('User');
 		if($this->User->Logged_in()) {
 			$this->Load_model('User_model');
-			$this->User_model->Update_user_activity($_SESSION['userid']);
+			$this->User_model->Update_user_activity($this->Session_get('userid'));
 		}
 	}
 	
