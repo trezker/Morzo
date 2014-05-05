523890031c05f754a5b991046fc9ac2f98542490
Anders Andersson <trezker@gmail.com>
2014-05-05 19:55:58 +0200

    Converted attach lock

--- controllers/actor.php
+++ controllers/actor.php
@@ -944,22 +944,29 @@ class Actor extends Base {
 	}
 
 	public function Attach_lock() {
-		$actor_id = $_POST['actor_id'];
+		$this->Load_controller('User');
+		if(!$this->User->Logged_in()) {
+			$this->Json_response_not_logged_in();
+		}
+
+		$actor_id = $this->Input_post('actor_id');
 
 		$this->Load_model('Actor_model');
-		if(!$this->Actor_model->User_owns_actor($_SESSION['userid'], $actor_id)) {
-			echo json_encode(array('success' => false, 'reason' => 'Not your actor'));
-			return;
+		if(!$this->Actor_model->User_owns_actor($this->Session_get('userid'), $actor_id)) {
+			return $this->Json_response_not_your_actor();
 		}
 		
-		$object_id = $_POST['object_id'];
-		$lock_id = $_POST['lock_id'];
+		$object_id = $this->Input_post('object_id');
+		$lock_id = $this->Input_post('lock_id');
 		
 		$this->Load_model('Inventory_model');
 		
 		$result = $this->Inventory_model->Attach_lock($actor_id, $object_id, $lock_id);
 
-		echo json_encode($result);
+		return array(
+			'type' => 'json',
+			'data' => $result
+		);
 	}
 
 	public function Detach_lock() {
