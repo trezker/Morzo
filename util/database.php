4234a5e3efedcf9bc075faf29b0588ed4593b25c
Anders Andersson <trezker@gmail.com>
2014-05-11 19:37:30 +0200

    Changed adodb to use mysqli to get rid of deprecation warning

--- util/database.php
+++ util/database.php
@@ -4,7 +4,7 @@ require_once '../libraries/adodb/adodb.inc.php';
 if(!function_exists ('Create_database_connection')) {
 	function Create_database_connection($config) {
 		if(!isset($db)) {
-			$db = ADONewConnection('mysqlt');
+			$db = ADONewConnection('mysqli');
 			$db->Connect($config['host'], $config['user'], $config['password'], $config['database']);
 			$db->Execute("set names 'utf8'");
 			$db->SetFetchMode(ADODB_FETCH_ASSOC);
