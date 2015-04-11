8d6c790c88dceb25befa77e87e957fa2a1634037
Anders Andersson <trezker@gmail.com>
2015-04-11 19:47:05 +0200

    Added replacement function for apache_request_headers when using nginx

--- framework/input.php
+++ framework/input.php
@@ -1,5 +1,21 @@
 <?php
 
+// apache_request_headers replacement for nginx
+if (!function_exists('apache_request_headers')) { 
+	function apache_request_headers() { 
+		foreach($_SERVER as $key=>$value) { 
+			if (substr($key,0,5)=="HTTP_") { 
+				$key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5))))); 
+				$out[$key]=$value; 
+			}
+			else {
+				$out[$key]=$value; 
+			}
+		} 
+		return $out; 
+	}
+}
+
 class Input {
 	private $get = array();
 	private $post = array();
